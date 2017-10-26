59bc69bcdd8a2f0d181f144228cbe935bf7a5939
vitoria ingred <vic0327.vi@gmail.com>
2017-10-26 10:58:58 -0200

    trabalho

---
+++ vitoria-local.js
@@ -0,0 +1,155 @@
+db.alunos.inserir(  { 
+    nome :  "Luana" , 
+   localização :  {  digite :  "Ponto" ,  coordenadas :  [-43.3160809,-15.8052074]  }  
+}  ); 
+db.alunos.inserir(  { 
+   nome :  "Joana D`arc" , 
+   localização :  {  digite :  "Ponto" ,  coordenadas:  [-43.310403,-157943479]  } 
+   
+}  ); 
+db.alunos.inserir(  { 
+   nome :  "Alberto Souza" , 
+   localização :  {  digite :  "Ponto" ,  coordenadas:  [-43.3179225,-15.8057438]  } 
+   
+}  ); 
+db.alunos.inserir(  { 
+   nome :  "Layla" , 
+   localização :  {  digite :  "Ponto" ,  coordenadas:  [-43.3179687,-15.8198619]  } 
+   
+}  );
+db.alunos.inserir(  { 
+   nome :  "Thalia Santos" , 
+   localização :  {  digite :  "Ponto" ,  coordenadas:  [ -43.341919,-15.792172]  } 
+   
+}  );
+db.alunos.inserir(  { 
+   nome :  "Emilly Raissa" , 
+   localização :  {  digite :  "Ponto" ,  coordenadas:  [-43.312611,-15.807591]  } 
+   
+}  );
+db.alunos.inserir(  { 
+   nome :  "Savia Fernandes" , 
+   localização :  {  digite :  "Ponto" ,  coordenadas:  [-43.3223997,-15.7814025]  } 
+   
+}  );
+db.alunos.inserir(  { 
+   nome :  "Dieyse" , 
+   localização :  {  digite :  "Ponto" ,  coordenadas:  [-43.323578,-15.781380]  } 
+   
+}  );
+db.alunos.inserir(  { 
+   nome :  "Vitoria Ingred" , 
+   localização :  {  digite :  "Ponto" ,  coordenadas:  [-43.314893,-15.817489]  } 
+   
+}  );
+db.alunos.inserir(  { 
+   nome :  "Joao Vitor" , 
+   localização :  {  digite :  "Ponto" ,  coordenadas:  [-43.306551,-15.8147075]  } 
+   
+}  );
+db.alunos.inserir(  { 
+   nome :  "Michelle" , 
+   localização :  {  digite :  "Ponto" ,  coordenadas:  [-43.3123914,-15.8263155]  } 
+   
+}  );
+db.alunos.inserir(  { 
+   nome :  "Maria Thais" , 
+   localização :  {  digite :  "Ponto" ,  coordenadas:  [-43.0288791,-15.7377377]  } 
+   
+}  );db.alunos.inserir(  { 
+   nome :  "Adrian Marcelo" , 
+   localização :  {  digite :  "Ponto" ,  coordenadas:  [-43.304557,-15.811190]  } 
+   
+}  );
+db.alunos.inserir(  { 
+   nome :  "Lara Mariana" , 
+   localização :  {  digite :  "Ponto" ,  coordenadas:  [-43.3153666,-15.8197566]  } 
+   
+}  );
+db.alunos.inserir(  { 
+   nome :  "Camila" , 
+   localização :  {  digite :  "Ponto" ,  coordenadas:  [-43.2960149,-15.8251198]  }
+   
+}  );
+db.alunos.inserir(  { 
+   nome :  "Livia Samara" , 
+   localização :  {  digite :  "Ponto" ,  coordenadas:  [-43.314748,-15.7953561]  } 
+   
+}  );
+db.alunos.inserir(  { 
+   nome :  "Micaelle Alves" , 
+   localização :  {  digite :  "Ponto" ,  coordenadas:  [-43.3148092,-15.8221044]  } 
+   
+}  );
+db.alunos.inserir(  { 
+   nome :  "Flora Mendes" , 
+   localização :  {  digite :  "Ponto" ,  coordenadas:  [-43.3051085,-15.8018395]  } 
+   
+}  );
+db.alunos.inserir(  { 
+   nome :  "Jade Passos" , 
+   localização :  {  digite :  "Ponto" ,  coordenadas:  [-43.309728,-15.819087]  } 
+   
+}  );
+db.alunos.inserir(  { 
+   nome :  "Sidney Emanuel" , 
+   localização :  {  digite :  "Ponto" ,  coordenadas:  [-43.3121088,-15.8036004]  } 
+   
+}  );
+db.alunos.inserir(  { 
+   nome :  "Lavinia" , 
+   localização :  {  digite :  "Ponto" ,  coordenadas:  [-43.3168428,-15.8045297]  } 
+   
+}  );
+db.alunos.inserir(  { 
+   nome :  "Victoria Felipe" , 
+   localização :  {  digite :  "Ponto" ,  coordenadas:  [-43.300751,-15.818761]  } 
+   
+}  );
+db.alunos.inserir(  { 
+   nome :  "emille Beatriz" , 
+   localização :  {  digite :  "Ponto" ,  coordenadas:  [-43.3125666,-15.8039344]  } 
+   
+}  );
+db.alunos.inserir(  { 
+   nome :  "Hellen Aline" , 
+   localização :  {  digite :  "Ponto" ,  coordenadas:  [-43.3165794,-15.7990549]  } 
+   
+}  );
+db.alunos.inserir(  { 
+   nome :  "Joyce Mriana" , 
+   localização :  {  digite :  "Ponto" ,  coordenadas:  [-43.3087511,-15.8290764]  } 
+   
+}  );
+db.alunos.inserir(  { 
+   nome :  "Amanda Pereira" , 
+   localização :  {  digite :  "Ponto" ,  coordenadas:  [-43.314899,-15.802350]  } 
+   
+}  );
+db.alunos.inserir(  { 
+   nome :  "Luan Alves" , 
+   localização :  {  digite :  "Ponto" ,  coordenadas:  [-43.31381338,-15.8007558]  } 
+   
+}  );
+db.alunos.inserir(  { 
+   nome :  "Joao Gabriel" , 
+   localização :  {  digite :  "Ponto" ,  coordenadas:  [-43.3049733,-15.8489078]  } 
+   
+}  );
+db.alunos.inserir(  { 
+   nome :  "Daniel Barbosa" , 
+   localização :  {  digite :  "Ponto" ,  coordenadas:  [-43.3084923,-15.805955]  } 
+   
+}  );
+db.alunos.inserir(  { 
+   nome :  "Tom Roberto" , 
+   localização :  {  digite :  "Ponto" ,  coordenadas:  [-43.3090244,-15.802596]  } 
+   
+}  );
+db.alunos.inserir(  { 
+   nome :  "Mercia" , 
+   localização :  {  digite :  "Ponto" ,  coordenadas:  [-43.0685685,-15.5343928]  } 
+   
+}  );
+
+
